75307396288ec4c1d8f2a193e773c77c094cd243
zarkonow <stojanov1987@gmail.com>
2025-04-29 01:52:56 +0200

    fix: Clean up formatting and improve readability in SafeLinkDirective

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -11,25 +11,25 @@ import { Directive, ElementRef, inject, input } from "@angular/core";
     }
 })
 export class SafeLinkDirective {
-    queryParam = input('myApp', {alias: 'appSafeLink'});
+    queryParam = input('myApp', { alias: 'appSafeLink' });
     private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
 
     constructor() {
-       console.log('SafeLinkDirective initialized');
-    
+        console.log('SafeLinkDirective initialized');
+
     }
 
     onConfirmLeavePage(event: MouseEvent) {
         const wantsToLeave = window.confirm('Are you sure you want to leave this page?');
-       
 
-        if(wantsToLeave){
-            const address = (event.target as HTMLAnchorElement).href;
-            (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+
+        if (wantsToLeave) {
+            const address = this.hostElementRef.nativeElement.href;
+            this.hostElementRef.nativeElement.href = address + '?from=' + this.queryParam();
             return
         }
 
         event.preventDefault(); // Prevent the default action of the link
-       }
+    }
 
 }
