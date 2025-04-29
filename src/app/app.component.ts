1a3c5d6569684edfeeef3e7005e5377ee370f572
zarkonow <stojanov1987@gmail.com>
2025-04-29 20:55:58 +0200

    feat: Add LogDirective for click logging and update components to utilize it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 
 import { AuthDirective } from './auth/auth.directive';
+import { LogDirective } from './log.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, AuthDirective],
+  imports: [AuthComponent, LearningResourcesComponent, AuthDirective, LogDirective],
 })
 export class AppComponent {}
